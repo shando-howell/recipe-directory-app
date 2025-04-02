969a6d1ac7dc577068a9652f38b59b7d45b06d8b
shandohowell <shandohowell94@gmail.com>
2025-04-02 05:14:22 -0500

    Working on home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar/Navbar';
 import Home from './pages/home/Home';
 import Create from './pages/create/Create';
 import Search from './pages/search/Search';
@@ -11,12 +12,19 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="/create">
             <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
           </Route>
         </Switch>
       </BrowserRouter>
