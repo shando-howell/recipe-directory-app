e052f4325c4f56c02e8ce1b591ce5dfa9dfee5da
shandohowell <shandohowell94@gmail.com>
2025-04-02 04:23:28 -0500

    Setting up the main components.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Search from './pages/search/Search';
+import Recipe from './pages/recipe/Recipe';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
