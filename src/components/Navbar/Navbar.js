2708114e264970273085cc16fe66d85e1288c1fc
shandohowell <shandohowell94@gmail.com>
2025-04-05 14:41:57 -0500

    Working on navbar theme component.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom"
+import { useTheme } from "../../hooks/useTheme";
+
 import { SearchBar } from "../SearchBar/SearchBar"
 
 import "./Navbar.css";
 
 const Navbar = () => {
+  const { color, changeColor } = useTheme()
+
   return (
-    <div className="navbar">
-        <nav>
+    <div className="navbar" style={{ background: color }}>
+        <nav onClick={() => changeColor('orange')}>
             <Link to="/" className="brand">
                 <h1>The Recipe</h1>
             </Link>
